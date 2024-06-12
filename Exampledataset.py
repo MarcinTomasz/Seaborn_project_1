331b4d1018c90da8eb0dac2fd3f659cddf937010
Marcint0masz <58958990+MarcinTomasz@users.noreply.github.com>
2024-06-12 11:08:31 +0200

    Update Exampledataset.py

--- Exampledataset.py
+++ Exampledataset.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #Load the dataset
-df=pd.read_csv("C:\\Users\\novyp\\Desktop\\abcd\\abcd.csv")
+#df=pd.read_csv("C:\\Users\\novyp\\Desktop\\abcd\\abcd.csv")
 df=pd.read_csv("https://github.com/MarcinTomasz/Seaborn_project_1/blob/7193421650e40c16067a1cbbdb8db9516a919793/abcd.csv")
 df.head()
 
